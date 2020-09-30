ce7edfbe946ce65f3961948fbeb98f2a38ddd081
cathyseo <cathyseo.d@gmail.com>
2020-09-30 12:13:09 -0400

    Upload
    Upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/*This is burger menu */
+(function() {
+  var body = document.body;
+  var burgerMenu = document.getElementsByClassName('b-menu')[0];
+  var burgerContain = document.getElementsByClassName('b-container')[0];
+  var burgerNav = document.getElementsByClassName('b-nav')[0];
+
+  burgerMenu.addEventListener('click', function toggleClasses() {
+    [body, burgerContain, burgerNav].forEach(function (el) {
+      el.classList.toggle('open');
+    });
+  }, false);
+})();
+
+/*This is prevent */
+const element = document.querySelector('form');
+element.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log('Form submission cancelled.');
+  alert ("You can not submit.");
+});
